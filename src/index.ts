5e73e835383d821636d107bdeac6f8bd970d922d
hwinkr <dnddl8280@naver.com>
2024-03-21 17:27:34 +0900

    deploy

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import "../templates/reset.css";
-import "../templates/common.css";
-
-import { $ } from "./utils/dom";
-import App from "./App";
-
-const root = $<HTMLDivElement>("#app");
-root && new App(root);
